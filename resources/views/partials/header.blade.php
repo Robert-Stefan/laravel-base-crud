fe81afe14601ece35df3d30e7b1dad6ea6281c43
Robert-Stefan <robertboss05@hotmail.it>
2021-06-14 17:44:37 +0200

    Add index & show

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -11,7 +11,7 @@
           <a class="nav-link" href="{{ route('home') }}">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="{{ route('comics.index') }}">Fumetti</a>
+          <a class="nav-link" href="{{ route('comics.index') }}">Comics</a>
         </li>
       </ul>
     </div>
